87d4332abbde88f2da5d5d45543715380eee7cee
supriadi <supriadiadi1001@gmail.com>
2024-05-29 08:38:08 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,12 +19,15 @@ export default function Home() {
   // Note: Define the type of event as React.ChangeEvent<HTMLInputElement>
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const rpmValue = parseInt(event.target.value, 10); // Convert the string to an integer
-    if (!isNaN(rpmValue)) {
-      setRpmMotor(rpmValue); // Set the rpm motor value if it's a valid number
+    if (!isNaN(rpmValue) && rpmValue <= 127) {
+      setRpmMotor(rpmValue); // Set the rpm motor value if it's a valid number and within the range
+    } else if (rpmValue > 127) {
+      setRpmMotor(127); // Set rpm motor to maximum value if input exceeds 127
     } else {
       setRpmMotor(undefined); // Set to undefined or reset if the conversion fails or field is cleared
     }
   };
+
   return (
     <Master>
       <div className="flex min-h-screen flex-col items-center p-24 gap-4">
